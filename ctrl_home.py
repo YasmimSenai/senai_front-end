93c948dd2739790a1d1d856fa29b86ae5bc8cdeb
Yasmim Senai <yasmim.mya13@gmail.com>
2026-05-11 12:37:25 -0300

    Apresentando dados na datatable

--- ctrl_home.py
+++ ctrl_home.py
@@ -22,6 +22,24 @@ def index(): # função que gerencia rota
 def dashboard(): # função que gerencia rota
     """ Painel de Vendas"""
     #if 'user' not in session:
-     #   return redirect(url_for("auth.login"))
+    #   return redirect(url_for("auth.login"))
+    import locale
+    #Define a localização para português brasileiro
+    locale.setlocale(locale.LC, 'pt_BR.UTF-8')
     
-    return render_template("dashboard/index.html", title="Painel de Vendas") # Renderiza um template
+    vendas: list = [
+        { "mes":"Janeiro", "total":139519.19},
+        { "mes":"Fevereiro", "total":125989.50},
+        { "mes":"Março", "total":128519.30},
+        { "mes":"Abril", "total":1399191.20},
+        { "mes":"Maio", "total":141611.90},
+        { "mes":"Junho", "total":142591.40},
+        { "mes":"Julho", "total":119996.13},
+        { "mes":"agosto", "total":769458.17},
+        { "mes":"Setembro", "total":824601.77},
+        { "mes":"Outrubro", "total":549342.66},
+        { "mes":"Novembro", "total":228746.69},
+        { "mes":"Dezembro", "total":453968.55},
+    ] #fim lista vendas
+    
+    return render_template("dashboard/index.html", vendas=vendas, locale=locale) # Renderiza um template
